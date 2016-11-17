b1bc1a6001af359f8b4eecd181ad2761c1e85102
GabrielVander <gabrielvanderlei10@hotmail.com>
2016-11-16 23:06:42 -0200

    Núcleo completamente desenvolvido
    Os sistemas principais do aplicativo foram remodelados e melhorados assim como foram adicionados arquivo únicos.

--- script.js
+++ script.js
@@ -1,20 +1,100 @@
+
+if(localStorage.getItem("rolls")){}
+else{
+    localStorage.setItem("rolls", 10)
+    localStorage.setItem("timePerRoll", 100)
+    localStorage.setItem("forcedRoll", 0)
+    localStorage.setItem("forcedRollNum", 0)
+    localStorage.setItem("forcedRollDisable", 0)
+    localStorage.setItem("forcedRollTimes", 0)
+}
+
+var rolls = localStorage.getItem("rolls"),
+    timePerRoll = localStorage.getItem("timePerRoll"),
+    forcedRoll = localStorage.getItem("forcedRoll"),
+    forcedRollNum = localStorage.getItem("forcedRollNum"),
+    forcedRollDisable = localStorage.getItem("forcedRollDisable"),
+    forcedRollTimes = localStorage.getItem("forcedRollTimes"),
+    forcedRollPlays = 0,
+    conf = 0,
+    tr = 0,
+    confPut;
+
 function random (min, max){
     max = Number(max);
     min = Number(min);
     return Math.floor(Math.random() * ((max - min) + 1)) + min;
 }
 
-var tr = 0;
 function roll(){
+    var max = Number(document.getElementById("max").value),
+        min = Number(document.getElementById("min").value);
+    
     var dr = setInterval(function(){
-        if(tr <= 10){
-            document.getElementById("result").innerHTML =           random(document.getElementById("min").value,document.getElementById("max").value);
+        if(tr <= rolls){
+            document.getElementById("result").innerHTML = random(min, max);
             tr++;
         }
         
         else{
             tr = 0;
+            if(forcedRoll == 1){
+                if((forcedRollNum <= max) && (forcedRollNum >= min)){
+                    document.getElementById("result").innerHTML = forcedRollNum;   }
+                if(forcedRollDisable == 1){
+                    forcedRollPlays++;
+                    if(forcedRollPlays >= forcedRollTimes){ 
+                        localStorage.setItem('forcedRoll', 0);
+                        forcedRoll = 0;forcedRollPlays = 0;}
+                }
+            }
             clearInterval(dr);
+            load();
         }
-    }, 100);
+    }, timePerRoll);
+    
+    
+}
+
+function sort(){
+    if(document.getElementById("tRand").style.left == "0%"){roll();}
+    document.getElementById("tRand").style.left = "0%";
+    document.getElementById("tConf").style.left = "100%";
+}
+
+function config(){
+    document.getElementById("tRand").style.left = "-100%";
+    document.getElementById("tConf").style.left = "0%";
+}
+
+load = function(){
+    confPut = document.getElementsByClassName("confPut");
+    for(i = 0; i< confPut.length; i++){
+        if(confPut[i]){
+            confPut[i].value = localStorage.getItem(confPut[i].id);
+            confPut[i].onchange = function(){
+                localStorage.setItem(this.id, Number(this.value));
+                
+                rolls = localStorage.getItem("rolls");
+                timePerRoll = localStorage.getItem("timePerRoll");
+                forcedRoll = localStorage.getItem("forcedRoll");
+                forcedRollNum = localStorage.getItem("forcedRollNum");
+                forcedRollDisable = localStorage.getItem("forcedRollDisable");
+                forcedRollTimes = localStorage.getItem("forcedRollTimes");
+                forcedRollPlays = 0;
+                conf = 0;
+                tr = 0;
+            }
+        }
+        else{
+            alert("Algum erro ocorreu, desculpe.");
+        }
+    }
+    
+    document.getElementById("tRand").style.left = "0%";
+    document.getElementById("tConf").style.left = "100%";
+}
+
+window.onload = function(){
+    load();
 }
